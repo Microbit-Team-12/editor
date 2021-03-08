81132f14cb40c3b61223190646db79cf65844f08
Ryosuke <korygm33@gmail.com>
2021-03-08 20:49:48 +0900

    implement minimal application layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
 import './App.css';
 
-function App(): JSX.Element {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+type AppState = {
+}
+
+const exampleDoc = `from microbit import *
+
+display.show(1)`;
+
+class App extends React.Component<unknown, AppState> {
+  constructor(props: unknown) {
+    super(props);
+    this.state = {};
+  }
+
+  render(): JSX.Element {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button className="App-button">Start</button>
+          <button className="App-button">Run Code</button>
+          <button className="App-button">Flash Code</button>
+          <button className="App-button">Interrupt</button>
+          <button className="App-button">Reboot</button>
+        </header>
+        <div className="App-textareas">
+          <textarea readOnly value={exampleDoc} className="App-doc"></textarea>
+          <textarea className="App-editor"></textarea>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
