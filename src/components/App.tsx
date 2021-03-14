58e163d510c487713ac8964103586c8827d17215
Jingjie Yang <jingjieyang2001@gmail.com>
2021-03-14 13:08:38 +0000

    Include code and docs in the app state

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,16 +2,40 @@ import React from 'react';
 import './App.css';
 
 type AppState = {
+  /** The code in the editor. */
+  code: string,
+  /** The markdown of the tutorial being displayed. */
+  docs: string,
 }
 
-const exampleDoc = `from microbit import *
+const exampleCode = `from microbit import *
 
 display.show(1)`;
+
+const exampleDocs = `# Title
+
+Did you know you can use tildes instead of backticks?
+
+~~~py
+# LINES 5-6
+from microbit import *
+
+while True:
+    display.scroll('Hello, World!')
+    display.show(Image.HEART)
+    sleep(2000)
+~~~
+
+More text
+`;
 
 class App extends React.Component<unknown, AppState> {
   constructor(props: unknown) {
     super(props);
-    this.state = {};
+    this.state = {
+      code: exampleCode,
+      docs: exampleDocs,
+    };
   }
 
   render(): JSX.Element {
@@ -25,8 +49,8 @@ class App extends React.Component<unknown, AppState> {
           <button className="App-button">Reboot</button>
         </header>
         <div className="App-textareas">
-          <textarea readOnly value={exampleDoc} className="App-doc"></textarea>
-          <textarea className="App-editor"></textarea>
+          <textarea readOnly className="App-doc">{this.state.docs}</textarea>
+          <textarea className="App-editor">{this.state.code}</textarea>
         </div>
       </div>
     );
