b7324ff5ad07657dcf22e4975d23a63f02754e2e
Georgia <70912730+gshave@users.noreply.github.com>
2021-03-18 20:55:10 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   
   return (
     <>
+      //example parameters to pass to a SpeechBubble object
       <SpeechBubble buttonsText={["syntaxError", "typeError"]} text={"What kind of error do you have?"} />
     </>
   ) 
