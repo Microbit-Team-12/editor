01b7dac9d8c8b254dbcbc44da5b79e12e36c64a9
Georgia <70912730+gshave@users.noreply.github.com>
2021-03-18 20:54:29 +0000

    Update SpeechBubble.js

--- src/SpeechBubble.js
+++ src/SpeechBubble.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 
 class SpeechBubble extends Component {
-  //render should be pure - doesn't modify component state
+  //render should be pure - shouldn't modify component state
   render() {
-    //examine this.props
-    //examine this.state
     const buttons = this.state.buttonsText
     const fullText = this.state.text
 
-    //returns html with the correct space for text and, 
-    //                  the correct number of buttons of the correct size 
+    //will return html with the correct space for text and, 
+    //                      the correct number of buttons of the correct size 
     return (
       <>
         <div>{fullText}</div>
